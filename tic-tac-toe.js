e95ea85c628b33e30163cd1ec4b46bfd3e0ed3b5
Marc-Anthony Mitchell <mitchellmarcus297@gmail.com>
2020-10-05 22:39:00 -0500

    JavaScript

---
+++ tic-tac-toe.js
@@ -0,0 +1,9 @@
+let s;
+window.onload=function(){
+    s=document.querySelector("#board");
+    s=s.querySelectorAll("div");
+    s.forEach(i =>{
+        i.setAttribute("class","square");
+    })
+   
+}
