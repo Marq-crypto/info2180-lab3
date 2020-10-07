8d5075ca0a6ee5ab06d0c1b8c5527fd5a3e76dba
Marc-Anthony Mitchell <mitchellmarcus297@gmail.com>
2020-10-07 00:02:56 -0500

    JavaScript

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,10 @@
 let s;
-var winner=[];
+let turn= 'X';
+let winner=[
+    '','','',
+    '','','',
+    '','','',
+];
 
 window.onload=function(){
     s=document.querySelector("#board");
@@ -9,29 +14,20 @@ window.onload=function(){
     })
    
 }
-s.forEach(i =>{
-    i.addEventListener("click",function(){
-        if(!(i.getAttribute("class").includes("X")||i.getAttribute("class").includes("O"))){
-            if (i==null){
-                i.setAttribute("class","square X");
-                i.innerHTML="X";
-                winner.push(i);
-            }
-            else if(i==null){
-                i.setAttribute("class","square O");
-                i.innerHTML="O";
-                winner.push(i);
-            }
-        }
-    })
-})
-s.forEach(i =>{
-    i.addEventListener("over",function(){
-        i.setAttribute("class","hover");
-    })
-})
-s.forEach(i =>{
-    i.addEventListener("leave",function(){
-        i.setAttribute("class","hover O");
-    })
-})
+const y=Array.from(s.querySelectorAll("#board div"));
+function mark(){
+    winner.forEach(function(m,i){
+        console.log(m,i);
+        y[i].textContent=m;
+    });
+};
+
+s.getElementById("board").addEventListener("click",handle);
+function handle(e){
+    let x=y.findIndex(function(square){
+        return square === e.target;
+    });
+    winner[x]=turn;
+    console.log(winner);
+    turn=turn==='X'?'O':'X';
+};
