85709ce09268d8b96bcc6bda7401ee4f68a27468
Marc-Anthony Mitchell <mitchellmarcus297@gmail.com>
2020-10-06 22:19:40 -0500

    JavaScript

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,7 @@
 let s;
+var winner=[];
+let check=false;
+
 window.onload=function(){
     s=document.querySelector("#board");
     s=s.querySelectorAll("div");
@@ -7,3 +10,21 @@ window.onload=function(){
     })
    
 }
+s.forEach(i =>{
+    s.addEventListener("click",function(){
+        if(!(s.getAttribute("class").includes("X")||s.getAttribute("class").includes("O"))){
+            if (check==false){
+                s.setAttribute("class","square X");
+                s.innerHTML="X";
+                winner.push(i);
+                check=true;
+            }
+            else if(check==true){
+                s.setAttribute("class","square O");
+                s.innerHTML="O";
+                winner.push(i);
+                check=false;
+            }
+        }
+    })
+})
